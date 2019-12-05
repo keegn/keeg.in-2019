84f02b33f7390b7c4ef9a02586498a889297a61b
Keegan Burkett <keegan.t.burkett@gmail.com>
2019-12-04 23:24:54 -0600

    Runs prettier

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,10 +4,10 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const NotFoundPage = () => (
-    <Layout>
-        <SEO title="404: Not found" />
-        <h1>NOT FOUND</h1>
-    </Layout>
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>NOT FOUND</h1>
+  </Layout>
 )
 
 export default NotFoundPage
