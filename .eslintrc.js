3ed1aed8f8e3b40f493f4830d7932f161d0f7ca6
Keegan Burkett <keegan.t.burkett@gmail.com>
2019-12-16 22:44:15 -0600

    Fix prettier eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,79 @@
+// module.exports = {
+//   parser: `@typescript-eslint/parser`,
+//   extends: [
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:prettier/recommended',
+//     'prettier/@typescript-eslint',
+//   ],
+//   plugins: ['@typescript-eslint', 'prettier'],
+//   parserOptions: {
+//     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+//     sourceType: 'module', // Allows for the use of imports
+//   },
+//   env: {
+//     browser: true,
+//     node: true,
+//   },
+//   rules: {
+//     quotes: 'off',
+//     '@typescript-eslint/quotes': [
+//       2,
+//       'backtick',
+//       {
+//         avoidEscape: true,
+//       },
+//     ],
+//     indent: ['error', 2, { SwitchCase: 1 }],
+//     'prettier/prettier': [
+//       'error',
+//       {
+//         trailingComma: 'es5',
+//         semi: true,
+//         singleQuote: true,
+//         printWidth: 120,
+//       },
+//     ],
+//   },
+// }
+
 module.exports = {
-  parser: `@typescript-eslint/parser`,
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
-  plugins: ['@typescript-eslint', 'prettier'],
-  parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
   env: {
     browser: true,
     node: true,
+    es6: true,
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
   },
   rules: {
-    quotes: 'off',
-    '@typescript-eslint/quotes': [
-      2,
-      'backtick',
-      {
-        avoidEscape: true,
-      },
-    ],
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'prettier/prettier': [
-      'error',
-      {
-        trailingComma: 'es5',
-        semi: true,
-        singleQuote: true,
-        printWidth: 120,
-      },
-    ],
+    'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
+    '@typescript-eslint/explicit-function-return-type': 'off',
   },
+  overrides: [
+    // Override some TypeScript rules just for .js files
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off', //
+      },
+    },
+  ],
 }
