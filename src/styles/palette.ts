4d5233df7340fedc4c30bc2c5d7e1b6173867886
keegn <keegan.t.burkett@gmail.com>
2019-12-22 16:38:33 -0700

    Tests dark mode module

---
+++ src/styles/palette.ts
@@ -0,0 +1,7 @@
+export const palette = {
+    darkAccent: "71,113,90",
+    darkShades: "48,41,23",
+    lightAccent: "156,219,220",
+    lightShades: "254,252,250",
+    mainBrand: "92,149,141",
+  }
