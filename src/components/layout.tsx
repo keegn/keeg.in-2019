9d95fc1c56f086c504c4c74a182b72951c45f19d
keegn <keegan.t.burkett@gmail.com>
2019-12-24 15:29:07 -0700

    Needs new dark mode solution

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,28 +10,34 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const [lightTheme, setLightTheme] = useState(true)
+  const [themeMode, setThemeMode] = useState('light')
 
   useEffect(() => {
-    const localStorageLayout = localStorage.getItem('lightTheme')
+    const localStorageLayout = localStorage.getItem('themeMode')
+    console.log('localStorageLayout ', localStorageLayout)
     if (localStorageLayout) {
-      setLightTheme(JSON.parse(localStorageLayout))
+      setThemeMode(localStorageLayout)
     }
   }, [])
 
   const changeTheme = () => {
-    setLightTheme(!lightTheme)
-    localStorage.setItem('lightTheme', !lightTheme)
+    if (themeMode === 'light') {
+      setThemeMode('dark')
+      localStorage.setItem('themeMode', 'dark')
+    } else if (themeMode === 'dark') {
+      setThemeMode('light')
+      localStorage.setItem('themeMode', 'light')
+    }
   }
 
-  console.log(lightTheme)
+  console.log('themeMode ', themeMode)
 
   return (
     <>
-      <ThemeProvider theme={lightTheme ? light : dark}>
+      <ThemeProvider theme={themeMode === 'light' ? light : dark}>
         <Sidebar href="/" />
         <GlobalStyle />
-        <Header changeTheme={changeTheme} lightTheme={lightTheme} />
+        <Header changeTheme={changeTheme} themeMode={themeMode} />
         {children}
       </ThemeProvider>
     </>
