878d3bc14fbd75a2c9daba8354910adf1b14a659
keegn <keegan.t.burkett@gmail.com>
2020-01-01 22:08:27 -0600

    Trys new window fix

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare let window: any
+const windowGlobal = typeof window !== 'undefined' && window
 
 const noop = () => {}
 // React hook for JS media queries
@@ -9,10 +10,11 @@ export const useMediaQuery = (query: any) => {
   // Fall back on dummy matchMedia in SSR.
   // eslint-disable-next-line no-undef
 
-  //   const theGlobalThis = globalThis
+  const theGlobalThis = windowGlobal
   const matchMedia =
     // eslint-disable-next-line no-undef
-    window.matchMedia || (() => ({ addListener: noop, removeListener: noop }))
+    theGlobalThis.matchMedia ||
+    (() => ({ addListener: noop, removeListener: noop }))
   query = matchMedia(query)
   const [matches, setMatches] = useState(query.matches)
   useEffect(() => {
