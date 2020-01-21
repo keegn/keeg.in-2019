aae9b5235924e3e94f783262102021387fea310d
keegn <keegan.t.burkett@gmail.com>
2020-01-20 23:50:34 -0600

    Adds framer motion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,112 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
+import { motion, useAnimation } from 'framer-motion'
 
 import Layout from '../components/layout'
 
 import SEO from '../components/seo'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Portfolio" />
-    <PageContainer>
-      <HeroText>Keegan Burkett</HeroText>
-      <P>
-        Hey, I'm Keegan. I'm a front-end software engineer based in Columbia,
-        MO. My current role at Paytient focuses on building consumer software
-        that helps folks pay for medical expenses over time, interest free.
-      </P>
-      <P>
-        I spend most of my day writing React/React Native apps. When I'm not
-        building software you can find me cruising the back roads of Missouri on
-        my gravel bike.
-      </P>
-      <LinksContainer>
-        <SocialLink target="blank" href="https://github.com/keegn">
-          Github
-        </SocialLink>
-        <SocialLink target="blank" href="https://dribbble.com/keegin">
-          Dribbble
-        </SocialLink>
-        <SocialLink target="blank" href="https://linkedin.com/in/keeganburkett">
-          LinkedIn
-        </SocialLink>
-      </LinksContainer>
-    </PageContainer>
-  </Layout>
-)
+const IndexPage = () => {
+  const controls = useAnimation()
+
+  useEffect(() => {
+    controls.start(i => ({
+      transition: { delay: i === 0 ? 0.2 : i * 0.3 },
+      opacity: 1,
+      y: 0,
+    }))
+  }, [])
+  return (
+    <Layout>
+      <SEO title="Portfolio" />
+      <PageContainer>
+        <HeroText
+          custom={0}
+          initial={{
+            opacity: 0,
+            y: 16,
+          }}
+          animate={controls}
+        >
+          Keegan Burkett
+        </HeroText>
+        <P
+          custom={0}
+          initial={{
+            opacity: 0,
+            y: 16,
+          }}
+          animate={controls}
+        >
+          Hey, I'm Keegan. I'm a front-end software engineer based in Columbia,
+          MO. My current role at Paytient focuses on building consumer software
+          that helps folks pay for medical expenses over time, interest free.
+        </P>
+        <P
+          custom={0}
+          initial={{
+            opacity: 0,
+            y: 24,
+          }}
+          animate={controls}
+        >
+          I spend most of my day writing React/React Native apps. When I'm not
+          building software you can find me cruising the back roads of Missouri
+          on my gravel bike.
+        </P>
+        <LinksContainer>
+          <SocialLink
+            target="blank"
+            href="https://github.com/keegn"
+            custom={1}
+            initial={{
+              opacity: 0,
+              y: 24,
+            }}
+            animate={controls}
+          >
+            Github
+          </SocialLink>
+          <SocialLink
+            target="blank"
+            href="https://dribbble.com/keegin"
+            custom={2}
+            initial={{
+              opacity: 0,
+              y: 24,
+            }}
+            animate={controls}
+          >
+            Dribbble
+          </SocialLink>
+          <SocialLink
+            target="blank"
+            href="https://linkedin.com/in/keeganburkett"
+            custom={3}
+            initial={{
+              opacity: 0,
+              y: 24,
+            }}
+            animate={controls}
+          >
+            LinkedIn
+          </SocialLink>
+        </LinksContainer>
+      </PageContainer>
+    </Layout>
+  )
+}
 
 const PageContainer = styled.main``
 
-const HeroText = styled.h1`
+const HeroText = styled(motion.h1)`
   padding: 60px 0 8px 0;
   font-family: ${props => props.theme.font.headlineExtra};
   font-size: 24px;
 `
 
-const P = styled.p`
+const P = styled(motion.p)`
   font-family: ${props => props.theme.font.paragraphLight};
 `
 
@@ -54,7 +116,7 @@ const LinksContainer = styled.div`
   flex-direction: column;
 `
 
-const SocialLink = styled.a`
+const SocialLink = styled(motion.a)`
   padding-bottom: 24px;
 `
 
