46e8e60399875e25bad33ae8e926b33a30d82391
keegn <keegan.t.burkett@gmail.com>
2020-02-15 19:54:41 -0600

    Adds form

--- src/components/chatWidget.tsx
+++ src/components/chatWidget.tsx
@@ -92,18 +92,13 @@ const ChatWidget: React.FC<Props> = () => {
                       </>
                     ) : (
                       <ContactForm
-                      name="contact"
-                      method="post"
-                      action="/thanks"
-                      data-netlify="true"
-                      data-netlify-honeypot="bot-field"
+                        name="contact"
+                        method="post"
+                        action="/thanks"
+                        data-netlify="true"
+                        data-netlify-honeypot="bot-field"
                       >
                         <input type="hidden" name="bot-field" />
-                        {/* <input
-                          type="hidden"
-                          name="form-name"
-                          value="contact"
-                        /> */}
                         <StyledInput
                           marginBottom
                           type="text"
@@ -128,36 +123,36 @@ const ChatWidget: React.FC<Props> = () => {
                           required
                         />
                         <button type="submit">Send Message</button>
-                        </ContactForm>
-                      {/* <form onSubmit={handleSubmit} name="contact" netlify>
-                        <StyledInput
-                          marginBottom
-                          type="text"
-                          placeholder="Your Name"
-                          name="name"
-                          onChange={handleChange}
-                          value={inputData.name}
-                          required
-                        />
-                        <StyledInput
-                          marginBottom
-                          type="email"
-                          placeholder="Your Email"
-                          name="email"
-                          onChange={handleChange}
-                          value={inputData.email}
-                          required
-                        />
-                        <StyledTextArea
-                          marginBottom
-                          placeholder="Your Message"
-                          name="message"
-                          onChange={handleChange}
-                          value={inputData.message}
-                          required
-                        />
-                        <button type="submit">Send Message</button>
-                      </form> */}
+                      </ContactForm>
+                      // {/* <form onSubmit={handleSubmit} name="contact" netlify>
+                      //   <StyledInput
+                      //     marginBottom
+                      //     type="text"
+                      //     placeholder="Your Name"
+                      //     name="name"
+                      //     onChange={handleChange}
+                      //     value={inputData.name}
+                      //     required
+                      //   />
+                      //   <StyledInput
+                      //     marginBottom
+                      //     type="email"
+                      //     placeholder="Your Email"
+                      //     name="email"
+                      //     onChange={handleChange}
+                      //     value={inputData.email}
+                      //     required
+                      //   />
+                      //   <StyledTextArea
+                      //     marginBottom
+                      //     placeholder="Your Message"
+                      //     name="message"
+                      //     onChange={handleChange}
+                      //     value={inputData.message}
+                      //     required
+                      //   />
+                      //   <button type="submit">Send Message</button>
+                      // </form> */}
                     )}
                   </BodyCardFooter>
                 </BodyCard>
@@ -203,7 +198,7 @@ const ChatConsole = styled.div`
   position: fixed;
   width: 380px;
   height: 75vh;
-  max-height: 400px;
+  max-height: 500px;
   bottom: 6rem;
   right: 16px;
   z-index: 320;
