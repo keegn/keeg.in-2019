e66b7c4748880e80a41c942801a574cadf7c7de6
keegn <keegan.t.burkett@gmail.com>
2020-02-16 11:41:32 -0600

    Removes form-name

--- src/components/chatWidget.tsx
+++ src/components/chatWidget.tsx
@@ -165,7 +165,6 @@ const ChatWidget: React.FC<Props> = () => {
                         data-netlify="true"
                         data-netlify-honeypot="bot-field"
                       >
-                        <input type="hidden" name="form-name" value="contact" />
                         <input type="hidden" name="bot-field" />
                         <StyledInput
                           marginBottom
