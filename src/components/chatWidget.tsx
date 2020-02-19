08cba3df28acc62c33d32e608e9855b9d4bfe5ff
keegn <keegan.t.burkett@gmail.com>
2020-02-18 20:46:35 -0600

    Fix spinner issue

--- src/components/chatWidget.tsx
+++ src/components/chatWidget.tsx
@@ -147,58 +147,40 @@ const ChatWidget: React.FC<Props> = () => {
                         )}
                       </>
                     ) : (
-                      <AnimatedFormContainer
-                        initial={{ y: -5 }}
-                        animate={{ y: 0 }}
-                        transition={{
-                          y: {
-                            type: 'spring',
-                            stiffness: 400,
-                            damping: 10,
-                          },
-                        }}
+                      <form
+                        name="contact"
+                        method="post"
+                        onSubmit={handleSubmit}
+                        data-netlify="true"
+                        data-netlify-honeypot="bot-field"
                       >
-                        <form
-                          name="contact"
-                          method="post"
-                          onSubmit={handleSubmit}
-                          data-netlify="true"
-                          data-netlify-honeypot="bot-field"
-                        >
-                          <input
-                            type="hidden"
-                            name="form-name"
-                            value="contact"
-                          />
-                          <input type="hidden" name="bot-field" />
-                          <input
-                            type="text"
-                            placeholder="Your Name"
-                            name="name"
-                            onChange={handleChange}
-                            value={inputData.name}
-                            required
-                          />
-                          <input
-                            type="email"
-                            placeholder="Your Email"
-                            name="email"
-                            onChange={handleChange}
-                            value={inputData.email}
-                            required
-                          />
-                          <textarea
-                            placeholder="Your Message"
-                            name="message"
-                            onChange={handleChange}
-                            value={inputData.message}
-                            required
-                          />
-                          <StyledButton type="submit">
-                            {isLoading ? <Spinner /> : 'Send Message'}
-                          </StyledButton>
-                        </form>
-                      </AnimatedFormContainer>
+                        <input type="hidden" name="form-name" value="contact" />
+                        <input type="hidden" name="bot-field" />
+                        <input
+                          type="text"
+                          placeholder="Your Name"
+                          name="name"
+                          onChange={handleChange}
+                          value={inputData.name}
+                          required
+                        />
+                        <input
+                          type="email"
+                          placeholder="Your Email"
+                          name="email"
+                          onChange={handleChange}
+                          value={inputData.email}
+                          required
+                        />
+                        <textarea
+                          placeholder="Your Message"
+                          name="message"
+                          onChange={handleChange}
+                          value={inputData.message}
+                          required
+                        />
+                        <StyledButton type="submit">Send Message</StyledButton>
+                      </form>
                     )}
                   </BodyCardFooter>
                 </BodyCard>
@@ -342,8 +324,6 @@ const BodyCardHeader = styled.div``
 const BodyCardBody = styled.div``
 
 const BodyCardFooter = styled.div``
-
-const AnimatedFormContainer = styled(motion.div)``
 
 const StyledButton = styled(motion.button)`
   padding: 0 12px;
